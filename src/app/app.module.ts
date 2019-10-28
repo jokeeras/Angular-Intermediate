7109a12286e7c206946b874704048cd97ae5111a
jokeeras <jokeeras@yahoo.com>
2019-10-28 17:37:46 +0200

    28.10.2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { 
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+ } from './events/index';
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { ToastrService } from './common/tostr.service';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { Error404Component } from './errors/404.component';
+import { AuthService } from './user/auth.service';
 
 
 @NgModule({
@@ -13,12 +24,31 @@ import { NavBarComponent } from './nav/navbar.component';
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent,
-
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    EventListResolver,
+    AuthService,
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState
+    }
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty) 
+    return window.confirm('You have not saved this event, do you really want to cancel?');
+  return true;
+}
--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { IEvent } from './shared/index';
 
 @Component({
   selector: 'event-thumbnail',
   template: `
-    <div class="well hoverwell thumbnail">
+    <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
       <h2>{{event.name}}</h2>
       <div>Date: {{event.date}}</div>
-      <div [ngSwitch]="event?.time">
-        Time: {{event.time}}
+      <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+        Time:  {{event.time}}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
         <span *ngSwitchDefault>(Normal Start)</span>
@@ -20,6 +21,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     </div>
   `,
   styles: [`
+    .green { color: #003300 !important; }
+    .bold { font-weight: bold; }
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
     .thumbnail { min-height: 210px; }
@@ -27,6 +30,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 
 export class EventThumbnailComponent {
-  @Input() event: any;
+  @Input() event: IEvent;
 
+  getStartTimeClass() {
+    const isEarlyStart = this.event && this.event.time === '8:00 am';
+    return {green: isEarlyStart, bold: isEarlyStart}
+    
+  }
 }
